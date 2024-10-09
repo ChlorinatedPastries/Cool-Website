9d212401a8247a29e4ac1c22870355432aeee76a
ChlorinatedPastries <chlorinatedpastries@proton.me>
2024-10-09 07:26:31 -0500

    g

--- tests/bad.js
+++ tests/bad.js
@@ -1,4 +1,11 @@
 function jkkk() {
 document.getElementById("demo").innerHTML = "21. <br> you stupid."
-}
 alert(413 + 69 - 420 * 1337 / 413);
+}
+
+function math(){
+const x = 812;
+const z = 1716;
+let y = x * z
+document.getElementById(math).innerHTML = "uu" + y;
+}
