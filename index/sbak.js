1711d5951fca6a4196331577c7f4722501298301
ChlorinatedPastries <chlorinatedpastries@proton.me>
2024-10-14 04:40:59 -0500

    ,,,

--- index/sbak.js
+++ index/sbak.js
@@ -1,1 +1,3 @@
-//help me "randomWord"
+const splashtext = ["chrosh","proxy","gamesite","Cookies","THE FACTORY MUST GROW", ]
+const randomElement = array[Math.floor(Math.random() * splashtext.length)];
+document.getElementById("splashtext").innerHTML = splashtext
--- tests/bad.js
+++ tests/bad.js
@@ -3,9 +3,11 @@ document.getElementById("demo").innerHTML = "21. <br> you stupid."
 alert(413 + 69 - 420 * 1337 / 413);
 }
 
-function math(){
 const x = 812;
 const z = 1716;
 let y = x * z
-document.getElementById(mathH).innerHTML = "69" + y;
-}
+document.getElementById("math").innerHTML = "69" + y;
+
+function Cookie(){
+const test = {hourswastedonCookiecliker:3887}
+document.getElementById("life").innerHTML = "I have " + test.hourswastedonCookiecliker + " hours wasted on Cookie cliker!" }
