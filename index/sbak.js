4f0f8608a1643148d1e775cc5ec2d439a99af867
ChlorinatedPastries <chlorinatedpastries@proton.me>
2024-10-19 07:54:39 -0500

    f
    d

--- index/sbak.js
+++ index/sbak.js
@@ -1,4 +1,4 @@
-const splashtext = ["chrosh","proxy","gamesite","Cookies","THE FACTORY MUST GROW", ]
+const splashtext = ["crosh","proxy","gamesite","Cookies","THE FACTORY MUST GROW", ]
 const randomElement = array[Math.floor(Math.random() * splashtext.length)];
 const uuu = randomElement
-document.getElementById("splashtext").innerHTML = splashtext
+document.getElementById("splashtext").innerHTML = uuu
