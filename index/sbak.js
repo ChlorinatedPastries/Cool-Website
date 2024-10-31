64dec35765f4d0930178e3cbec9cdc902f0745eb
ChlorinatedPastries <chlorinatedpastries@proton.me>
2024-10-30 19:38:38 -0500

    f

--- index/sbak.js
+++ index/sbak.js
@@ -1,10 +1,5 @@
-
-
-(function () {
-    const fruits = ["crosh","proxy","gamesite","Cookies","THE FACTORY MUST GROW","" ];
+   const fruits = ["crosh","proxy","gamesite","Cookies","THE FACTORY MUST GROW","" ];
     const randomIndex = Math.floor(Math.random() * fruits.length);
     const randomFruit = fruits[randomIndex];
     console.log(randomFruit);
     document.getElementById("splashtext").innerHTML = "NNNSDSDS";
-  })();
-
