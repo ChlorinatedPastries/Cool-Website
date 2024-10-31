275b9e32fe7080dedb5b937a8435a40007255777
ChlorinatedPastries <chlorinatedpastries@proton.me>
2024-10-31 03:10:22 -0500

    junk

--- tests/bad.js
+++ tests/bad.js
@@ -12,8 +12,21 @@ function Cookie(){
 const test = {hourswastedonCookiecliker:3887}
 document.getElementById("life").innerHTML = "I have " + test.hourswastedonCookiecliker + " hours wasted on Cookie cliker!" }
 
+
+function coool() {
 const fruits = ["crosh","proxy","gamesite","Cookies","THE FACTORY MUST GROW", ];
 const randomIndex = Math.floor(Math.random() * fruits.length);
 const randomFruit = fruits[randomIndex];
 console.log(randomFruit);
-document.getElementById("splashtext").innerHTML = "NNNSDSDS";
+document.getElementById("splashtext").innerHTML = randomFruit ;
+}
+(function() {
+    console.log("This function runs automatically!");
+    const fruits = ["crosh","proxy","gamesite","Cookies","THE FACTORY MUST GROW", ];
+    const randomIndex = Math.floor(Math.random() * fruits.length);
+    const randomFruit = fruits[randomIndex];
+    console.log(randomFruit);
+    document.getElementById("splashtext").innerHTML = randomFruit ;
+    document.getElementById("demo").innerHTML = "ogogogog"
+})();
+document.getElementById("demo").innerHTML
