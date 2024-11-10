7d659e685b770b746238af605c963a1263ba3f19
ChlorinatedPastries <chlorinatedpastries@proton.me>
2024-11-10 08:09:34 -0600

    junk

--- index/sbak.js
+++ index/sbak.js
@@ -1,5 +1,5 @@
-   const fruits = ["crosh","proxy","gamesite","Cookies","THE FACTORY MUST GROW","Lorem ipsum dolor sit amet, <br> consectetur adipisicing elit. <br> Adipisci, ad illum consequatur consequuntur similique <br> quidem dignissimos possimus ipsa sequi nisi, <br> cupiditate voluptatibus id debitis,  <br> deserunt non nam provident quia porro!" ];
-    const randomIndex = Math.floor(Math.random() * fruits.length);
-    const randomFruit = fruits[randomIndex];
-    console.log(randomFruit);
-    document.getElementById("splashtext").innerHTML = "NNNSDSDS";
+const fruits = ["crosh","proxy","gamesite","Cookies","THE FACTORY MUST GROW","Lorem ipsum dolor sit amet, <br> consectetur adipisicing elit. <br> Adipisci, ad illum consequatur consequuntur similique <br> quidem dignissimos possimus ipsa sequi nisi, <br> cupiditate voluptatibus id debitis,  <br> deserunt non nam provident quia porro!" ];
+const randomIndex = Math.floor(Math.random() * fruits.length);
+const randomFruit = fruits[randomIndex];
+console.log(randomFruit);    
+document.getElementById("demo").innerHTML = randomFruit;
