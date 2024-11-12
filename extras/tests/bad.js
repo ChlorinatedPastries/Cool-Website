956a30e6612d1e6a7177fb7d1a8adb020ff673f7
ChlorinatedPastries <chlorinatedpastries@proton.me>
2024-11-11 19:22:51 -0600

    dasd

--- extras/tests/bad.js
+++ extras/tests/bad.js
@@ -1,32 +1,3 @@
-function jkkk() {
-document.getElementById("demo").innerHTML = "21. <br> you stupid."
-alert(413 + 69 - 420 * 1337 / 413);
+function cool() {
+    document.getElementById("demo").innerHTML = "<i>21.</i> <br> you stupid."
 }
-
-const x = 812;
-const z = 1716;
-let y = x * z
-document.getElementById("math").innerHTML = "69" + y;
-
-function Cookie(){
-const test = {hourswastedonCookiecliker:3887}
-document.getElementById("life").innerHTML = "I have " + test.hourswastedonCookiecliker + " hours wasted on Cookie cliker!" }
-
-
-function coool() {
-const fruits = ["crosh","proxy","gamesite","Cookies","THE FACTORY MUST GROW", ];
-const randomIndex = Math.floor(Math.random() * fruits.length);
-const randomFruit = fruits[randomIndex];
-console.log(randomFruit);
-document.getElementById("splashtext").innerHTML = randomFruit ;
-}
-(function() {
-    console.log("This function runs automatically!");
-    const fruits = ["crosh","proxy","gamesite","Cookies","THE FACTORY MUST GROW", ];
-    const randomIndex = Math.floor(Math.random() * fruits.length);
-    const randomFruit = fruits[randomIndex];
-    console.log(randomFruit);
-    document.getElementById("splashtext").innerHTML = randomFruit ;
-    document.getElementById("demo").innerHTML = "ogogogog"
-})();
-document.getElementById("demo").innerHTML
