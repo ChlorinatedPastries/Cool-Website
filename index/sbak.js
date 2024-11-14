f7bac11012ea4b2acdfcecd04191d66579dab99e
ChlorinatedPastries <chlorinatedpastries@proton.me>
2024-11-13 18:30:00 -0600

    tried to make the random text work
    error "Uncaught TypeError: Cannot set properties of null (setting 'innerHTML')"
    buggy code: "document.getElementById("innerHTML").innerHTML = result;"

--- index/sbak.js
+++ index/sbak.js
@@ -1,5 +1,7 @@
-const fruits = ["crosh","proxy","gamesite","Cookies","THE FACTORY MUST GROW","Lorem ipsum dolor sit amet, <br> consectetur adipisicing elit. <br> Adipisci, ad illum consequatur consequuntur similique <br> quidem dignissimos possimus ipsa sequi nisi, <br> cupiditate voluptatibus id debitis,  <br> deserunt non nam provident quia porro!" ];
+const fruits = ["Cookies","THE FACTORY MUST GROW","Lorem ipsum dolor sit amet, <br> consectetur adipisicing elit. <br> Adipisci, ad illum consequatur consequuntur similique <br> quidem dignissimos possimus ipsa sequi nisi, <br> cupiditate voluptatibus id debitis,  <br> deserunt non nam provident quia porro!"
+    ,"(defun botsbuildbots () (botsbuildbots))" , ""
+ ];
 const randomIndex = Math.floor(Math.random() * fruits.length);
-const randomFruit = fruits[randomIndex];
-console.log(randomFruit);    
-document.getElementById("demo").innerHTML = randomFruit;
+const result = fruits[randomIndex];
+console.log(result);    
+document.getElementById("innerHTML").innerHTML = 5 + 6
