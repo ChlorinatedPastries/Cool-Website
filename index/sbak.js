f15a1dee893cc6d460d3f9248e87da3e9044ce1c
ChlorinatedPastries <chlorinatedpastries@proton.me>
2024-11-20 19:02:12 -0600

    COOL

--- index/sbak.js
+++ index/sbak.js
@@ -1,5 +1,5 @@
 const fruits = ["Cookies","THE FACTORY MUST GROW","Lorem ipsum dolor sit amet, <br> consectetur adipisicing elit. <br> Adipisci, ad illum consequatur consequuntur similique <br> quidem dignissimos possimus ipsa sequi nisi, <br> cupiditate voluptatibus id debitis,  <br> deserunt non nam provident quia porro!"
-    ,"(defun botsbuildbots () (botsbuildbots))" , ""
+    ,"(defun botsbuildbots () (botsbuildbots))" , "I CENTER DIV EZ YIPPYYYYYYYYYYYYYYYYYYYYYYYY"
  ];
 const randomIndex = Math.floor(Math.random() * fruits.length);
 const result = fruits[randomIndex];
