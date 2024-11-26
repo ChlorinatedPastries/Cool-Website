a281a5548916f44c80b87785fe8fd5717e35e3f8
ChlorinatedPastries <chlorinatedpastries@proton.me>
2024-11-26 07:54:13 -0600

    img

--- index/sbak.js
+++ index/sbak.js
@@ -1,7 +1,9 @@
-const fruits = ["Cookies","THE FACTORY MUST GROW","Lorem ipsum dolor sit amet, <br> consectetur adipisicing elit. <br> Adipisci, ad illum consequatur consequuntur similique <br> quidem dignissimos possimus ipsa sequi nisi, <br> cupiditate voluptatibus id debitis,  <br> deserunt non nam provident quia porro!"
-    ,"(defun botsbuildbots () (botsbuildbots))" , "I CENTER DIV EZ YIPPYYYYYYYYYYYYYYYYYYYYYYYY"
- ];
-const randomIndex = Math.floor(Math.random() * fruits.length);
-const result = fruits[randomIndex];
-console.log(result);    
-document.getElementById("innerHTML").innerHTML = 5 + 6
+function idklol() {
+    const fruits = ["Cookies","THE FACTORY MUST GROW","Lorem ipsum dolor sit amet, <br> consectetur adipisicing elit. <br> Adipisci, ad illum consequatur consequuntur similique <br> quidem dignissimos possimus ipsa sequi nisi, <br> cupiditate voluptatibus id debitis,  <br> deserunt non nam provident quia porro!"
+        ,"(defun botsbuildbots () (botsbuildbots))" , "I CENTER DIV EZ YIPPYYYYYYYYYYYYYYYYYYYYYYYY"
+     ];
+    const randomIndex = Math.floor(Math.random() * fruits.length);
+    const result = fruits[randomIndex];
+    console.log(result);    
+    document.getElementById("color").innerHTML = result   
+}
