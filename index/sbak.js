f1a5ab54f8562a3a0933c22ed0061198a16fa93d
ChlorinatedPastries <chlorinatedpastries@proton.me>
2024-12-01 08:18:57 -0600

    dx

--- index/sbak.js
+++ index/sbak.js
@@ -1,6 +1,6 @@
 function idklol() {
     const fruits = ["Cookies","THE FACTORY MUST GROW","Lorem ipsum dolor sit amet, <br> consectetur adipisicing elit. <br> Adipisci, ad illum consequatur consequuntur similique <br> quidem dignissimos possimus ipsa sequi nisi, <br> cupiditate voluptatibus id debitis,  <br> deserunt non nam provident quia porro!"
-        ,"(defun botsbuildbots () (botsbuildbots))" , "I CENTER DIV EZ YIPPYYYYYYYYYYYYYYYYYYYYYYYY" , "Hey bro, got any  <I><u><b>UPSHOW</b></u></i>"
+        ,"(defun botsbuildbots () (botsbuildbots))" , "I CENTER DIV EZ YIPPYYYYYYYYYYYYYYYYYYYYYYYY" , "Hey bro, got any  <I><u><b>UPSHOW</b></u></i>?", "", "Made with the Shapez 2 soundtrack!", "Welcome to the Information Superhighway!",
      ];
     const randomIndex = Math.floor(Math.random() * fruits.length);
     const result = fruits[randomIndex];
